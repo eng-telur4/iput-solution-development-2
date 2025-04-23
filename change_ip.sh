377ca303c0c238e6fbca25b36f6cb6d7b7b1f485
Shotaro Kamijo <eng.telur4@gmail.com>
2025-04-23 10:13:07 +0900

    Update change_ip.sh

--- change_ip.sh
+++ change_ip.sh
@@ -6,15 +6,19 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
-# すべての既存リッチルールを削除
+# "ssh" サービスを対象にした rich rule だけを削除
 sudo firewall-cmd --zone=public --list-rich-rules | while read -r rule; do
-    echo "Removing: $rule"
-    sudo firewall-cmd --zone=public --remove-rich-rule="$rule" --permanent
+    if echo "$rule" | grep -q "service name=\"ssh\""; then
+        echo "Removing SSH rule: $rule"
+        sudo firewall-cmd --zone=public --remove-rich-rule="$rule" --permanent
+    else
+        echo "Keeping non-SSH rule: $rule"
+    fi
 done
 
-# 引数で指定されたすべてのIPに対してルールを追加
+# 引数で指定されたすべてのIPに対して SSH のルールを追加
 for IP in "$@"; do
-    echo "Adding rule for $IP"
+    echo "Adding SSH rule for $IP"
     sudo firewall-cmd --zone=public --add-rich-rule="rule family='ipv4' source address='$IP' service name='ssh' accept" --permanent
 done
 
